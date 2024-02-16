9dabb67be6aeb1f7741fb34d9b24a4b968140244
George Hill <georgeophill@outlook.com>
2024-02-16 17:26:30 +0000

    changed zoom

---
+++ src/pages/parentWork/ParentAboutTalks.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ParentAboutTalks.css";
+import ParentAboutTalksPage from "../../components/ParentWork/ParentAboutTalksPage";
+
+const ParentAboutTalks = () => {
+  return (
+    <div>
+      <ParentAboutTalksPage />
+    </div>
+  );
+};
+
+export default ParentAboutTalks;
